62c9a9ed7dc5a00c9cc2d0b4d8fb181f7783c786
Anderson Moura Clementino <anderson.moura@msn.com>
2019-01-23 16:41:42 -0200

    Refactor
    Refactor Controller Clientes

--- brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.algaworks.brewer.model.Cliente;
 
 @Controller
-public class ClienteController {
+public class ClientesController {
 
 	@RequestMapping("/clientes/novo")
 	public String novo(Cliente cliente) {
