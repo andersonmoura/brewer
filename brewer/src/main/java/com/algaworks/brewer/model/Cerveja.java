28024f4a2ec2780d0a573370897bfde4b3a6f32c
Anderson Moura Clementino <anderson.moura@msn.com>
2019-01-26 13:51:05 -0200

    JPA
    Adicionado dependências JPA/HIBERNATE
    Realizado mapeamento JPA Cerveja/Estilo/Sabor/Origem

--- brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,10 +1,34 @@
 package com.algaworks.brewer.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-public class Cerveja {
+@Entity
+@Table(name = "cerveja")
+public class Cerveja implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
@@ -15,8 +39,25 @@ public class Cerveja {
 	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
 	private String descricao;
 	
+	private BigDecimal valor;
 	
+	@Column(name = "teor-alcoolico")
+	private BigDecimal teorAlcoolico;
 	
+	private BigDecimal comissao;
+	
+	@Column(name = "quantidade-estoque")
+	private Integer quantidadeEstoque;
+	
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
+	
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	
+	@ManyToOne
+	@JoinColumn(name = "codigo_estilo")
+	private Estilo estilo;
 	
 	public String getDescricao() {
 		return descricao;
@@ -36,6 +77,78 @@ public class Cerveja {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public BigDecimal getTeorAlcoolico() {
+		return teorAlcoolico;
+	}
+	public void setTeorAlcoolico(BigDecimal teorAlcoolico) {
+		this.teorAlcoolico = teorAlcoolico;
+	}
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	public Origem getOrigem() {
+		return origem;
+	}
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+	public Sabor getSabor() {
+		return sabor;
+	}
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+	public Estilo getEstilo() {
+		return estilo;
+	}
+	public void setEstilo(Estilo estilo) {
+		this.estilo = estilo;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 	
 	
 	
---
+++ brewer/src/main/java/com/algaworks/brewer/model/Sabor.java
@@ -0,0 +1,27 @@
+package com.algaworks.brewer.model;
+
+public enum Sabor {
+
+	ADOCICADA("Adocicada"),
+	AMARGA("Amarga"),
+	FORTE("Forte"),
+	FRUTADA("Frutada"),
+	SUAVE("Suave");
+	
+	private String descricao;
+	
+	Sabor(String descricao){
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
